7ee380e4a0b17a1b49ff3891ce4d689afb41549d
zechocapic <zechocapic@gmail.com>
2013-08-11 00:46:01 +0200

    last commit before implementing game engine

--- MyAndroidGame/src/main/java/com/zechocapic/myandroidgame/GameCar.java
+++ MyAndroidGame/src/main/java/com/zechocapic/myandroidgame/GameCar.java
@@ -11,7 +11,7 @@ import android.graphics.Paint;
  * Created by zechocapic on 30/07/13.
  */
 public class GameCar {
-    private static String TAG = GameCar.class.getSimpleName();
+    //private static String TAG = GameCar.class.getSimpleName();
 
     public final static int MOVE_CAR_LEFT = -1;
     public final static int MOVE_CAR_RIGHT = 1;
@@ -42,7 +42,7 @@ public class GameCar {
 
     public void draw(Canvas canvas) {
         //canvas.drawRect(xPos - 32, yPos - 48, xPos + 32, yPos + 48, carPaint);
-        canvas.drawBitmap(bitmap, xPos - (bitmap.getWidth() /2), yPos, null);
+        canvas.drawBitmap(bitmap, xPos - (bitmap.getWidth() / 2), yPos -(bitmap.getHeight() / 2), null);
         //Log.d(TAG, "x = " + xPos + " ; y = " + yPos);
     }
 
